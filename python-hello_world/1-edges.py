8419d5cffcd5ed7370559dc34074b7f6c80d71dd
Kurabachew Chanyalew <aegood872@gmail.com>
2023-07-19 10:33:59 -0700

    python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,1 +1,4 @@
- print(' First 3 letters: Hol\nLast 2 letters: on\nMiddle word: olberto')
+word= "Holberton"
+print("First 3 letters:",word[0:3])
+print ("Last 2 letters:",word[-2:])
+print("Middle word:",word[1:-1]) 
---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,8 @@
+  number = int(input("Enter a number: "))
+if number >= 0:
+   if number == 0:
+       print(number,"is zero")
+   else:
+       print(number ,"is positive")
+else:
+   print(number,"is negative")
