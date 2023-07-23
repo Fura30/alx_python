8f2d2f3bc82283baffea4a78b54922f008c686b5
Kurabachew Chanyalew <aegood872@gmail.com>
2023-07-23 04:57:16 -0700

    last

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,17 +1,32 @@
+#!/usr/bin/env python3
+import re
+
 def validate_password(password):
+    # Check if the password is at least 8 characters long
     if len(password) < 8:
         return False
-    has_uppercase = any(char.isupper() for char in password)
-    has_lowercase = any(char.islower() for char in password)
-    has_digit = any(char.isdigit() for char in password)   
-    if not (has_uppercase and has_lowercase and has_digit):
-        return False    
-    if  ' ' in password:
-        return False    
+
+    # Check if the password contains at least one uppercase letter
+    if not re.search(r[A-Z], password):
+        return False
+
+    # Check if the password contains at least one lowercase letter
+    if not re.search(r[a-z], password):
+        return False
+
+    # Check if the password contains at least one digit
+    if not re.search(rd, password):
+        return False
+
+    # Check if the password contains any spaces
+    if   in password:
+        return False
+
+    # If all checks passed, return True
     return True
-password1 = "SecurePass123"
-password2 = "weakpass"
-password3 = "pass with spaces"
-print(validate_password(password1)) 
-print(validate_password(password2)) 
-print(validate_password(password3))
+
+# Test cases
+print(validate_password("Password123"))  # True
+print(validate_password("abc123"))  # False (no uppercase letter)
+print(validate_password("Password 123"))  # False (contains spaces)
+print(validate_password("password123"))  # False (no uppercase letter)
