bef9fddb150c121a28b6fc231921852c8038081a
Kurabachew Chanyalew <aegood872@gmail.com>
2023-07-25 07:17:37 -0700

    python module

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,7 +8,7 @@ def main():
     if num_args == 0:
         print("0 arguments.")
     else:
-        print(f"{num_args} {argument if num_args == 1 else arguments}:")
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
         for i, arg in enumerate(args, start=1):
             print(f"{i}: {arg}")
 
