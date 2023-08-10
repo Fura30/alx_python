ac6ef634acbe8deab2c2a576155d2190f547b2d7
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-10 13:13:14 +0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,9 @@
-class Base:    
+class Base:
     __nb_objects = 0  # private class attribute
 
-    def __init__(self id=None):
-
-        if id is not None:  # if id is given assign it to the instance attribute id
+    def __init__(self, id=None):
+        if id is not None:
             self.id = id
-        else:  # if id is not given increment __nb_objects and assign it to the instance attribute id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
