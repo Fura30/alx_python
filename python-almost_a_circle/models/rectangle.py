224433130a287fad9d27a83994089d2b1bda6b94
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-11 16:27:37 +0300

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,59 +1,41 @@
-#!/usr/bin/python3
-class Base:
-    __nb_objects = 0  # private class attribute
+from base import Base
 
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         self.__height = value
-        
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         self.__x = value
-        
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         self.__y = value
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)        ￼Enter
