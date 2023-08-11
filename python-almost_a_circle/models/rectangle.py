bbc9904740461edc0d348cabb38adf506f23372d
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-11 16:33:58 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,4 @@
+
 from base import Base
 
 class Rectangle(Base):
@@ -38,4 +39,16 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.__y = value
+        self.__y = value 
+
+class Rectangle( Rectangle):
+ if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)      
