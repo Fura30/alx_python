f59753e317012a0a79e4e1a5d27eb5de9025c0d3
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-13 16:53:10 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,2 +1,18 @@
+#checking instance of object in each class
 def is_same_class(obj, a_class):
-    return type(obj) is a_class
+#    print(".")
+   return type(obj) is a_class
+
+# Classes 
+class A:
+    pass
+
+class B(A):
+    pass
+
+obj1 = A()
+obj2 = B()
+
+is_same_class(obj1, A)
+a = 1
+#print (  is_same_class(a, int))
