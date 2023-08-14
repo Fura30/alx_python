f36822ef319ab65b3d817bb78f7933a2855b229b
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 17:49:10 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
+"""
+Checks if the given object is exactly an instance of the specified class.
+"""
 def is_same_class(obj, a_class):
-    return type(obj) is a_class
+    """
+    Checks if the given object is exactly an instance of the specified class.
 
-a = 1
-if is_same_class(a, int):
-  
-if is_same_class(a, float):
-  
-if is_same_class(a, object):
-  
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of the specified class, otherwise False.
+    """
+    return type(obj) is a_class
