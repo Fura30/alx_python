a9c8a6a45a757e0bc68d52a2d2d1881ea9d31980
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 15:57:37 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,31 +1,12 @@
-#!/usr/bin/env python3
-"""
-this code asigns the size of a square 0 unless the the user defines it
-"""
-class Square:
-    """
-    This class defines a square by a private instance attribute 'size'.
+#!/usr/bin/python3
 
-    Attributes:
-        __size (int): The size of the square.
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
 
-    Methods:
-        __init__(self, size=0): Initializes a new Square instance with the given size.
-    """
-
-    def __init__(self, size=0):
-        """
-        Initializes a new Square instance with the given size.
-
-        Args:
-            size (int, optional): The size of the square. Defaults to 0.
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
