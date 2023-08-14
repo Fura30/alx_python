9b540f7d9a174a921938ad7d6afe50fd22fe40f7
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 16:40:37 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -5,8 +5,8 @@ def is_same_class(obj, a_class):
 
 a = 1
 if is_same_class(a, int):
-    print(True)
+    print()
 if is_same_class(a, float):
-    print(False)
+    print()
 if is_same_class(a, object):
-    print(False)
+    print()
