2ba1315b888cf935ac401f6141816dd6934a1b07
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 18:01:00 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,1 +1,32 @@
 
+"""
+created an empty class
+"""
+class BaseMeta(type):
+     """
+     created a BaseMeta class
+     """
+     def __dir__(self):
+        x = super().__dir__()
+        list= [item for item in x if item != "__init_subclass__"]
+        return list
+     
+class BaseGeometry(metaclass=BaseMeta):
+    """
+    created a class called BaseGeometry
+    """
+    def __dir__(self):
+        x = super().__dir__()
+        list= [item for item in x if item != "__init_subclass__"]
+        return list
+    
+    def area(self):
+        """
+        this function raises an exception 
+        """
+        raise Exception("area() is not implemented")
+
+
+
+
+    
