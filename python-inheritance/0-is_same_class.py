dea30ff676bf4985a14571f88b685cf40c7170c4
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 15:44:38 +0300

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,31 @@
-def is_same_class(obj, a_class):
+#!/usr/bin/env python3
+"""
+this code asigns the size of a square 0 unless the the user defines it
+"""
+class Square:
     """
-    Returns True if the object is exactly an instance of the specified class; otherwise False.
+    This class defines a square by a private instance attribute 'size'.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance with the given size.
     """
-    return type(obj) == a_class
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
