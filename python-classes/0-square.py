b38087d443dd3a547e6ac6093533f011ce89babf
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-14 17:30:35 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,24 @@
+#!/usr/bin/env python3
+"""
+This code allows the user to define the size of a square
+"""
 class Square:
+    """
+    This class defines a square by a private instance attribute 'size'.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
-        
+  
