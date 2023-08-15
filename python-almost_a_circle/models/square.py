6267bd80bca2af9133e438ee5f8c45dec45caa6d
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-15 17:10:40 +0300

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,38 +1,57 @@
-"""
-This module imports the rectangle class
-"""
-from models.rectangle import Rectangle
+#!/usr/bin/python3
+"""This module defines the rectangle class."""
 
+from models.rectangle import Rectangle
 """
-imported class rectangle
+A class returning the square of the rectangle
 """
 class Square(Rectangle):
     """
-    this class inherits the Rectangle class
+    This class represents a rectangle.
+
+    Attributes:
+       __width (int): The width of the rectangle
+       __height (int): The height of the rectangle
+       __x (int): the x-coordinate of the rectangles position
+       __y (int): the y-coordinate of the rectangles position
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
-        This  method takes in the size and cordinate of a square
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The heigth of the rectangle
+            x (int):  The x-coordinate of the rectangle's position
+            y (int): The y-coordinate of the rectangle's position
+            id(int): The unique identifier of the rectangle 
         """
-        super().__init__(size, size, x=0, y=0, id=None)
+
+        super().__init__(size, size, x, y, id)
+
+
+    
+
+    @property
+    def size(self):
+        """getter for width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set value of width to height"""
+
+        if not isinstance(value, int):
+            raise TypeError ("width must be an integer")
+
+        if value >=0:            
+            self.width = self.height=  value
+        else:
+            raise ValueError("width must be > 0")
 
     def __str__(self):
         """
-        This method overides the other methods
+        returning area of Square
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return  f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
     
-    @property
-    def size(self):
-        """
-        This getter method is used to retrive values of the attribute width
-        """
-        return self.width
-    @size.setter
-    
-    def size(self, value):
-        """
-        This setter method modifies the value of the width
-        """
-        self.width = value
-
