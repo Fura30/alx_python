7d16baa72f46aaf8bba292f12b547950e20fe411
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-08-18 18:10:51 +0300

    Update 4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,41 +1,27 @@
-#!/urs/bin/env/python3
+"""
+Sends a request to a URL and displays the response body. Prints an error message with
+the HTTP status code if the status code is greater than or equal to 400.
+"""
 
 import requests
 import sys
 
-"""
-errorcode takes Url , send request to Url and display the body
-"""
-def errorcode(url):
-    """
-    Fetches the content of a URL and displays the response body.
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
     
-    If the HTTP status code is greater than or equal to 400, an error message
-    is printed along with the error code.
+    url = sys.argv[1]
     
-    Args:
-        url (str): The URL to send a request to.
-    """
-
-    try:
-        response = requests.get(url)
-        print("Response Body: ")
-        print(repsonse.text)
-
-        if reponse.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-        
-        # if response.status_code < 400:
-        #     print("Regular request")
-
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)  
-
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Display the response body
+    print(response.text)
+    
+    # Check if the status code is greater than or equal to 400
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please provide a URL as an argument.")
-    else:
-        url = sys.argv[1]
-        errorcode(url)
+    main()
