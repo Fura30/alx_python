115e69c3bb7fe58b6d003a261497314dfc0ab9c6
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-09-01 16:30:20 +0300

    Update 0-hello_route.py

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-#import  Flask class
+"""
+This is a simple Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    """
+    Route: /
+    Displays "Hello HBNB!" on the homepage.
+    """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
