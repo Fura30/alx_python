65661dd07e88c6e0b13c3ae5600934d5cba4448e
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-09-03 13:57:52 +0300

    Update 3-python_route.py

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -2,36 +2,36 @@
 """
 This is a simple Flask web application.
 """
+ 
+from flask import Flask
 
-from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display 'HBNB'"""
     return "HBNB"
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c_text(text):
     """Display 'C ' followed by the value of the text variable"""
-    return "C {}".format(text.replace('_', ' '))
+    # Replace underscore symbols with a space
+    text = text.replace('_', ' ')
+    return f"C {text}"
 
-
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def display_python_text(text="is cool"):
-     """Display 'Python ' followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
+def python_text(text):
+    """Display 'Python ' followed by the value of the text variable (default is 'is cool')"""
+    # Replace underscore symbols with a space
+    text = text.replace('_', ' ')
+    return f"Python {text}"
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
  
