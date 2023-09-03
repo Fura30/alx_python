7f977323efc51b6487786518ea994689067aff0c
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-09-03 12:16:13 +0300

    Update 1-hbnb_route.py

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+This is a simple Flask web application.
+"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
-  """
-    Route: /
-    Displays "Hello HBNB!" on the homepage.
-    """
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display 'HBNB'"""
     return "HBNB"
-  """
-    Route: /hbnb
-    Displays "HBNB on the homepage.
-    """
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+ 
