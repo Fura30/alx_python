185dd11470123b7383f2fa3b1aeb2c85c35521b2
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-10-01 18:02:00 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,37 @@
+
 import csv
 import requests
 import sys
 
-def fetch_employee_data(employee_id):
-    # Define the API endpoints
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
+if len(sys.argv) != 2:
+  print("Usage: python3 export_to_CSV.py USER_ID")
+  sys.exit(1)
 
-    # Fetch employee data
-    response_employee = requests.get(employee_url)
-    employee_data = response_employee.json()
-    employee_name = employee_data.get("username")
+user_id = sys.argv[1]
 
-    # Fetch TODO list data
-    response_todo = requests.get(todo_url)
-    todo_list = response_todo.json()
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+if response.ok:
+  user = response.json()
+else:
+  print(f"Error: could not retrieve information for user {user_id}")
+  sys.exit(1)
 
-    return employee_name, todo_list
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
+if response.ok:
+  todos = response.json()
+else:
+  print(f"Error: could not retrieve tasks for user {user_id}")
+  sys.exit(1)
 
-def export_to_csv(employee_id, employee_name, todo_list):
-    # Define the CSV filename
-    csv_filename = f"{employee_id}.csv"
+filename = f"{user_id}.csv"
 
-    # Write data to the CSV file
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+with open(filename, "w", newline="") as csvfile:
+  writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+  writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+  for todo in todos:
+    completed = "True" if todo["completed"] else "False"
+    writer.writerow([user["id"], user["username"], completed, todo["title"]])
 
-        for task in todo_list:
-            csv_writer.writerow([employee_id, employee_name, str(task["completed"]), task["title"]])
+#print(f"Exported {len(todos)} tasks for user {user['username']} to {filename}")
 
-    print(f"Data has been exported to {csv_filename}")
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    employee_name, todo_list = fetch_employee_data(employee_id)
-
-    # Export data to CSV
-    export_to_csv(employee_id, employee_name, todo_list)
-
-if __name__ == "__main__":
-    main()
- 
