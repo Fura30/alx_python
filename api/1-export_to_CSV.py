73b17ef12583b50706dbf2fb2919f2268ff2c5c9
Fura30 <137708657+Fura30@users.noreply.github.com>
2023-10-02 16:35:23 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,37 +1,44 @@
 
 import csv
 import requests
-import sys
-
-if len(sys.argv) != 2:
-  print("Usage: python3 export_to_CSV.py USER_ID")
-  sys.exit(1)
-
-user_id = sys.argv[1]
-
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
-if response.ok:
-  user = response.json()
-else:
-  print(f"Error: could not retrieve information for user {user_id}")
-  sys.exit(1)
-
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
-if response.ok:
-  todos = response.json()
-else:
-  print(f"Error: could not retrieve tasks for user {user_id}")
-  sys.exit(1)
-
-filename = f"{user_id}.csv"
-
-with open(filename, "w", newline="") as csvfile:
-  writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-  writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-  for todo in todos:
-    completed = "True" if todo["completed"] else "False"
-    writer.writerow([user["id"], user["username"], completed, todo["title"]])
-
-#print(f"Exported {len(todos)} tasks for user {user['username']} to {filename}")
+from sys import argv
 
 
+def export_to_csv(employee_id):
+    """
+    Export the tasks owned by the employee with the given ID to a CSV file.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        None
+    """
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    tasks_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    employee_response = requests.get(user_url)
+    tasks_response = requests.get(tasks_url)
+
+    employee_data = employee_response.json()
+    tasks_data = tasks_response.json()
+
+    employee_name = employee_data.get("username")
+    employee_tasks = []
+
+    for task in tasks_data:
+        task_completed = task.get("completed")
+        task_title = task.get("title")
+        employee_tasks.append([employee_id, employee_name, str(task_completed), task_title])
+
+    csv_filename = f"{employee_id}.csv"
+
+    with open(csv_filename, "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csvwriter.writerows(employee_tasks)
+
+
+if __name__ == "__main__":
+    employee_id = int(argv[1])
+    export_to_csv(employee_id)
